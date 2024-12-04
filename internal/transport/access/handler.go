b17db44422a7ca63c0898dd7e2d67d8aadcf41c2
Nikita Brusnikin <134952903+Mobo140@users.noreply.github.com>
2024-12-04 20:08:25 +0300

    add logger to access api

--- internal/transport/access/handler.go
+++ internal/transport/access/handler.go
@@ -5,8 +5,11 @@ import (
 	"errors"
 	"strings"
 
+	"github.com/Mobo140/auth/internal/logger"
 	"github.com/Mobo140/auth/internal/service"
 	desc "github.com/Mobo140/auth/pkg/access_v1"
+	"go.uber.org/zap"
+
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -29,24 +32,52 @@ func NewImplementation(accessService service.AccessService) *Implementation {
 func (i *Implementation) Check(ctx context.Context, req *desc.CheckRequest) (*emptypb.Empty, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return nil, errors.New("metadata is not provided")
+		err := errors.New("metadata is not provided")
+		logger.Error("Failed to get metadata from context: ",
+			zap.Any("request", req),
+			zap.Error(err),
+		)
+
+		return nil, err
 	}
 
 	authHeader, ok := md["authorization"]
 	if !ok || len(authHeader) == 0 {
-		return nil, errors.New("authorization header is not provided")
+		err := errors.New("authorization header is not provided")
+		logger.Error("Failed to get auth header from metadata: ",
+			zap.Any("request", req),
+			zap.Error(err),
+		)
+
+		return nil, err
 	}
 
 	if !strings.HasPrefix(authHeader[0], authPrefix) {
-		return nil, errors.New("invalid authorization header format")
+		err := errors.New("invalid authorization header format")
+		logger.Error("Failed to get prefix Bearer: ",
+			zap.Any("request", req),
+			zap.Error(err),
+		)
+
+		return nil, err
 	}
 
 	accessToken := strings.TrimPrefix(authHeader[0], authPrefix)
 
 	err := i.accessService.Check(ctx, accessToken, req.GetEndpointAddress())
 	if err != nil {
+		logger.Error("Failed to check: ",
+			zap.Any("endpoint", req.GetEndpointAddress()),
+			zap.Any("access token", accessToken),
+			zap.Error(err),
+		)
+
 		return nil, err
 	}
+
+	logger.Info("User successfully authenticated and granted access",
+		zap.String("endpoint", req.GetEndpointAddress()),
+	)
 
 	return &emptypb.Empty{}, nil
 }
