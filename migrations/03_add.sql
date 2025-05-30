f758c4c224ccd706720c38c1b14be1b025943d63
Nikita Brusnikin <134952903+Mobo140@users.noreply.github.com>
2025-05-30 09:10:32 +0300

    update

--- migrations/03_add.sql
+++ migrations/03_add.sql
@@ -1,9 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE client ADD COLUMN hash_password VARCHAR(255) SET NOT NULL;
--- +goose StatementEnd
+ALTER TABLE
+    client
+ADD
+    COLUMN hash_password VARCHAR(255) NOT NULL;
 
+-- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE client DROP COLUMN hash_password;
+ALTER TABLE
+    client DROP COLUMN hash_password;
+
 -- +goose StatementEnd
